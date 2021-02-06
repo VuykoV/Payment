b425bb268b49a3dc40c13a4caa997ac6458f1c92
VuykoV <VuykoVlad@gmail.com>
2021-02-06 19:51:53 +0200

    new commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            top: 100,
+            left: 100
+        };
+    }
+
+    random(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    yes () {
+        alert('мы так и думали')
+    };
+
+    no =() => {
+        this.setState({
+            top: this.random(window.screen.width),
+            left: this.random(window.screen.height)
+        })
+    };
+
+    render() {
+
+        const {top, left} = this.state;
+
+    return (
+      <div className="div">
+        <text children="вас устраивает ваша зарплата?" className="question"/>
+        <button
+            children="да"
+            onClick={this.yes}
+        />
+        <button
+            children="нет"
+            style={{top, left}}
+            onMouseEnter={this.no}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
